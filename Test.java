53d27d6873216dd3afe240b405b0c85695d8f550
hussain3573 <67228751+hussain3573@users.noreply.github.com>
2020-06-21 16:51:36 +0300

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,15 @@
+
+package test;
+
+
+public class Test {
+
+   
+    public static void main(String[] args) {
+        
+        fast f1=new fast();
+        f1.setVisible(true);
+        
+    }
+    
+}
